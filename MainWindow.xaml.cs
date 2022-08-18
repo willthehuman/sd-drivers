4f04742b1f9362333be9d334a364788006ea7b2e
Will <williamcperrier@gmail.com>
2022-08-18 07:49:03 -0400

    using neptune api

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Windows;
 using Nefarius.ViGEm.Client;
+using neptune_hidapi.net;
+
 namespace sd_drivers
 {
     /// <summary>
@@ -10,10 +13,27 @@ namespace sd_drivers
     {
         static readonly ViGEmClient client = new();
         static readonly IVirtualGamepad virtualGamepad = client.CreateXbox360Controller();
+        static readonly NeptuneController neptune = new();
         public MainWindow()
         {
             InitializeComponent();
-            virtualGamepad.Connect();
+            //virtualGamepad.Connect();
+            neptune.OnControllerInputReceived += neptune_OnControllerInputReceived;
+            neptune.LizardButtonsEnabled = false; //Mouse and Keyboard emulation enabled.
+            neptune.LizardMouseEnabled = true;
+            neptune.Open();
+        }
+
+        private async Task neptune_OnControllerInputReceived(NeptuneControllerInputEventArgs arg)
+        {
+            foreach (var btn in arg.State.ButtonState.Buttons)
+            {
+                Console.WriteLine($"{btn}: {arg.State.ButtonState[btn]}      ");
+            }
+            foreach (var axis in arg.State.AxesState.Axes)
+            {
+                Console.WriteLine($"{axis}: {arg.State.AxesState[axis]}      ");
+            }
         }
     }
 }
