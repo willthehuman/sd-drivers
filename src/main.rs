6b987c1e2949ea68c75ef39e8a8a707848295ad0
willthehuman <williamcperrier@gmail.com>
2022-09-29 13:34:48 -0400

    Steam Deck Driver Rust Reborn (SDDRR)

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+extern crate hidapi;
+extern crate eframe;
+extern crate keymapper;
+
+use eframe::egui;
+use hidapi::HidApi;
+use keymapper::*;
+
+fn main() {
+    let hidapi = HidApi::new().unwrap();
+    //TODO: modify main() and overload load_profiles to a config file path
+    //TODO: pass full profile path to keymapper
+    keymapper::main();
+
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "My egui App",
+        options,
+        Box::new(|_cc| Box::new(MyApp::default())),
+    );
+}
+
+struct MyApp {
+    name: String,
+    age: u32,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            name: "Arthur".to_owned(),
+            age: 42,
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("My egui Application");
+            ui.horizontal(|ui| {
+                ui.label("Your name: ");
+                ui.text_edit_singleline(&mut self.name);
+            });
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            if ui.button("Click each year").clicked() {
+                self.age += 1;
+            }
+            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+        });
+    }
+}
