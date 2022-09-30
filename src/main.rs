f3ab66f8644eb9f71995cf01f4e2740b1c21a706
William Charbonneau Perrier <williamcperrier@gmail.com>
2022-09-30 14:04:57 -0400

    start/stop drivers

--- src/main.rs
+++ src/main.rs
@@ -3,51 +3,69 @@ extern crate hidapi;
 extern crate eframe;
 extern crate keymapper;
 
+use std::error::Error;
 use eframe::egui;
 use hidapi::HidApi;
+use tokio::task::JoinHandle;
 use keymapper::*;
+use std::future::IntoFuture;
 
-fn main() {
-    let hidapi = HidApi::new().unwrap();
-    //TODO: modify main() and overload load_profiles to a config file path
-    //TODO: pass full profile path to keymapper
-    keymapper::main();
-
+#[tokio::main]
+async fn main() {
     let options = eframe::NativeOptions::default();
     eframe::run_native(
         "My egui App",
         options,
-        Box::new(|_cc| Box::new(MyApp::default())),
+        Box::new(|_cc| Box::new(MyApp::new())),
     );
 }
 
 struct MyApp {
-    name: String,
-    age: u32,
+    hidapi: HidApi,
+    is_driver_running: bool,
+    driver_handle: Option<JoinHandle<()>>,
 }
 
-impl Default for MyApp {
-    fn default() -> Self {
+impl MyApp {
+    fn new() -> Self {
         Self {
-            name: "Arthur".to_owned(),
-            age: 42,
+            hidapi: HidApi::new().unwrap(),
+            is_driver_running: false,
+            driver_handle: None,
         }
     }
 }
 
 impl eframe::App for MyApp {
+
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
+
             ui.heading("My egui Application");
-            ui.horizontal(|ui| {
-                ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name);
-            });
-            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            if ui.button("Click each year").clicked() {
-                self.age += 1;
+            let mut toggle_button = ui.button((if self.is_driver_running {"Stop"} else {"Start"}).to_string());
+
+            if toggle_button.clicked() {
+                if self.is_driver_running {
+                    // kill the driver
+                    if let Some(driver_handle) = self.driver_handle.take() {
+                        driver_handle.abort();
+                        println!("Driver stopped");
+                        self.is_driver_running = false;
+                    }
+
+                } else {
+                    let handle = tokio::spawn(async {
+                        let v = async { keymapper::main() };
+                        let mut fut = v.into_future();
+                        fut.await;
+                    });
+
+                    self.driver_handle = Some(handle);
+                    self.is_driver_running = true;
+                    println!("Driver started");
+                }
             }
-            ui.label(format!("Hello '{}', age {}", self.name, self.age));
         });
     }
 }
+
