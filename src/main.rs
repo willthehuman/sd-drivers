6624cb4262dc5b45ef8b7aa475b99af8fc20583c
William Charbonneau <williamcperrier@gmail.com>
2022-10-07 23:04:30 -0400

    reading from buffer!!!

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
-extern crate hidapi;
 extern crate eframe;
 extern crate keymapper;
+extern crate rusb;
 
 use std::error::Error;
 use eframe::egui;
-use hidapi::HidApi;
 use tokio::task::JoinHandle;
 use keymapper::*;
 use std::future::IntoFuture;
+use rusb::{Device, DeviceHandle, GlobalContext, InterfaceDescriptors, UsbContext};
+use rusb::ffi::{libusb_claim_interface, libusb_handle_events, libusb_ss_usb_device_capability_descriptor};
 
 #[tokio::main]
 async fn main() {
@@ -21,15 +22,28 @@ async fn main() {
 }
 
 struct MyApp {
-    hidapi: HidApi,
+    neptune: DeviceHandle<GlobalContext>,
     is_driver_running: bool,
     driver_handle: Option<JoinHandle<()>>,
 }
 
 impl MyApp {
     fn new() -> Self {
+        let mut neptune = rusb::open_device_with_vid_pid(0x28de, 0x1205).unwrap();
+        let duration = std::time::Duration::from_millis(1000);
+        let language = neptune.read_languages(duration).unwrap()[0];
+        let res = neptune.claim_interface(0x84);
+        println!("claim interface result: {:?}", res);
+
+        for i in 0..=u8::MAX {
+            let res = neptune.claim_interface(i);
+            if let Ok(desc) = neptune.read_string_descriptor(language, i, duration) {
+                println!("claim interface {:?} result: {:?}", res, desc);
+            }
+        }
+
         Self {
-            hidapi: HidApi::new().unwrap(),
+            neptune,
             is_driver_running: false,
             driver_handle: None,
         }
@@ -39,6 +53,18 @@ impl MyApp {
 impl eframe::App for MyApp {
 
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+
+        let mut rbuf = [0u8; 64];
+        for i in 0..=u8::MAX {
+            //clear terminal
+            print!("{esc}c", esc = 27 as char);
+            let _ = self.neptune.read_interrupt(i, &mut rbuf, std::time::Duration::from_millis(500));
+            println!("read interrupt {:?} result: {:?}", i, rbuf);
+        }
+
+
+        println!("{:x?}", rbuf);
+
         egui::CentralPanel::default().show(ctx, |ui| {
 
             ui.heading("My egui Application");
